b3e607f60eeda7fc8806ecc4342570b3de9ac686
toma222 <aidannwilhite@gmail.com>
2024-01-06 07:32:41 +0000

    added a premake5

---
+++ premake5.lua
@@ -0,0 +1,28 @@
+
+project "box2d"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.cpp"
+		"include/**.hpp"
+	}
+
+	includes
+	{
+		"include"
+	}
+
+	filter "configurations:Debug"
+		defines "DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "NDEBUG"
+		runtime "Release"
+		optimize "on"
